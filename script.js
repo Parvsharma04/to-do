c061f8c84310b301cc6221c793eb87c7b6351b7d
Parv Sharma <sharmaparv.2004@gmail.com>
2024-08-09 20:01:01 +0530

    Add Delete

--- script.js
+++ script.js
@@ -23,35 +23,31 @@ function addTask() {
     setData(data);
   }
   showTask(taskInfo);
-  window.location.reload()
+  taskInput.value = '';
+  window.location.reload();
 }
 
 let doneBtn = document.querySelectorAll(".status");
 
 doneBtn.forEach((ele) => {
-  ele.addEventListener("click", taskDone);
+  ele.addEventListener("click", taskStateChange);
 });
 
-function taskDone(e) {
+function taskStateChange(e) {
   let data = getData();
   if (data === null) return;
-  let task = e.target.parentNode.firstChild;
-  let taskStatus;
-  data.map((ele) => {
-    if (ele.id == e.target.parentNode.classList[0]) {
-      ele.completed = !ele.completed;
-      taskStatus = ele.completed;
-    }
-  });
+  let taskId = e.target.parentNode.classList[0];
+    let task = data.find(ele => ele.id == taskId);
 
-  doneBtn.innerHTML = taskStatus ? "Not Done" : "Done";
-  task.innerHTML = taskStatus ? `<s style="background-color:lightgreen; color: black;">${task.innerText}</s>` : task.innerText;
-  console.log(data);
-  setData(data);
-  window.location.reload()
+    if (task) {
+        task.completed = !task.completed;
+        setData(data);
+        renderTasks();  // Re-render tasks to update the display
+    }
 }
 
 function renderTasks() {
+  taskList.innerHTML = ''
   let data = getData();
   if (data === null) return;
   console.log(data);
@@ -60,18 +56,46 @@ function renderTasks() {
   });
 }
 
+let deleteBtn = document.querySelectorAll(".delete");
+deleteBtn.forEach((ele) => {
+  ele.addEventListener("click", deleteTask);
+});
+console.log(deleteBtn)
+
+function deleteTask(e) {
+  let data = getData();
+  if (data === null) return;
+  for (let i = 0; i < data.length; i++)
+    if (data[i].id == e.target.parentNode.classList[0]) data.splice(i, 1);
+  let ID = 1;
+  data.forEach((ele) => {
+    ele.id = ID++;
+  });
+  setData(data);
+  renderTasks()
+}
+
 function showTask(ele) {
   let newTask = document.createElement("li");
   let Task = document.createElement("span");
   let taskDone = document.createElement("button");
+  let Delete = document.createElement("button");
   taskDone.innerText = ele.completed ? "Not Done" : "Done";
   taskDone.setAttribute("class", `status ${ele.id}`);
   newTask.setAttribute("class", `${ele.id}`);
-  Task.innerHTML = !ele.completed ? ele.name : `<s style="background-color:lightgreen; color: black;">${ele.name}</s>`;
+  Delete.setAttribute("class", "delete");
+  Delete.innerText = "Delete";
+  Task.innerHTML = !ele.completed
+    ? ele.name
+    : `<s style="background-color:lightgreen; color: black;">${ele.name}</s>`;
 
   newTask.append(Task);
   newTask.append(taskDone);
+  newTask.append(Delete);
   taskList.append(newTask);
+
+  taskDone.addEventListener("click", taskStateChange);
+  Delete.addEventListener("click", deleteTask);
 }
 
 function getData() {
